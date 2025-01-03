97d094b466d70f49a09cda08f88807c3aea888a5
Deive_Oliveira <deive.anjos.ext@unimednacional.coop.br>
2025-01-03 17:03:55 -0300

    populando banco de dados com Migrations e Aplicando o Data Annotations

---
+++ APICatalogo/APICatalogo/Migrations/20250103194932_PopulaProdutos.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APICatalogo.Migrations
+{
+    public partial class PopulaProdutos : Migration
+    {
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("Insert into Produtos(Nome,Descricao,Preco,ImagemUrl,Estoque,DataCadastro,CategoriaId)" +
+                    "Values ('Coca-Cola','Refrigerante de cola 350 ml', 5.45, 'cocacola.jpg',50,now(),1)");
+
+            mb.Sql("Insert into Produtos(Nome,Descricao,Preco,ImagemUrl,Estoque,DataCadastro,CategoriaId)" +
+                    "Values ('Lanche de Carne','Lanche de carne com queijo 50g', 15.55, 'lancheCarne.jpg',15,now(),2)");
+
+            mb.Sql("Insert into Produtos(Nome,Descricao,Preco,ImagemUrl,Estoque,DataCadastro,CategoriaId)" +
+                    "Values ('Pudim','Pudim de doce de Leite 100g', 12.45, 'pudim.jpg',25,now(),3)");
+        }
+
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("Delete from Produtos");
+        }
+    }
+}
--- APICatalogo/APICatalogo/Models/Categoria.cs
+++ APICatalogo/APICatalogo/Models/Categoria.cs
@@ -1,11 +1,17 @@
 ﻿using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace APICatalogo.Models;
 
     public class Categoria
     {
+        [Key]
         public int CategoriaId { get; set; }
+        [Required]
+        [StringLength(80)]
         public string? Nome { get; set; }
+        [Required]
+        [StringLength(300)]
         public string? ImagemUrl { get; set; }
 
         public ICollection<Produto> ? Produtos { get; set; }
